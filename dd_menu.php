4da5a8c53894d2434ae20f8771be5826da10dc5f
courtney-jo <courtneyjo.boortz@gmail.com>
2017-12-04 18:15:24 -0500

    Cafe Menu Changes

---
+++ dd_menu.php
@@ -0,0 +1,48 @@
+<?php
+
+include 'mydb.php';
+
+$mysqli = NEW MySQLi('localhost', 'root', '', 'cafefinder');
+
+$result = $mysqli->query("SELECT * FROM drinks WHERE Cafe='Dunkin donuts'");
+?>
+
+
+<!DOCTYPE html>
+{% extends "header.html" %}
+{% block content %}
+
+<html>
+<body bgcolor="#995533">
+    <h1 align="center">Cafe Name</h1>
+    <table border="1" align="center" style="line-height:25px;">
+        <tr>
+            <th>NAME</th>
+            <th>Price</th>
+            <th>Strength</th>
+            <th>Iced/Hot</th>
+			<th>Size</th>
+        </tr>
+        <?php
+        if($result->num_rows != 0){
+        while($row = $result->fetch_assoc()){
+        ?>
+        <tr>
+            <td><?php echo $row['Type']; ?></td>
+            <td><?php echo $row['Price']; ?></td>
+			<td><?php echo $row['Strength']; ?></td>
+            <td><?php echo $row['Iced_Hot']; ?></td>
+            <td><?php echo $row['Size']; ?></td>
+        </tr>
+        <?php
+        }
+        }
+        else{
+        echo"no results";
+        }
+
+        ?>
+    </table>
+</body>
+</html>
+{% endblock %}
---
+++ luckygoat_menu.php
@@ -0,0 +1,46 @@
+<?php
+
+include 'mydb.php';
+
+$mysqli = NEW MySQLi('localhost', 'root', '', 'cafefinder');
+
+$result = $mysqli->query("SELECT * FROM `drinks`,`cafes` WHERE `Cafe`='Lucky Goat' AND `NAME`= 'Lucky Goat'");
+?>
+
+
+<!DOCTYPE html>
+{% extends "header.html" %}
+{% block content %}
+
+<html>
+<body bgcolor="#995533">
+<?php
+        if($result->num_rows != 0)
+			$row = $result->fetch_assoc();
+        ?>
+    <h1 align="center">Lucky Goat Coffee</h1>
+	<h3 align="center"><?php echo $row['Hours_of_operation']; ?></h3>
+	<h3 align="center"><?php echo $row['Address']; ?></h3>
+    <table border="1" align="center" style="line-height:25px;">
+        <tr>
+            <th>NAME</th>
+            <th>Price</th>
+            <th>Strength</th>
+            <th>Iced/Hot</th>
+			<th>Size</th>
+        </tr>
+        <?php
+        do{
+        ?>
+        <tr>
+            <td><?php echo $row['Type']; ?></td>
+            <td><?php echo $row['Price']; ?></td>
+			<td><?php echo $row['Strength']; ?></td>
+            <td><?php echo $row['Iced_Hot']; ?></td>
+            <td><?php echo $row['Size']; ?></td>
+        </tr>
+        <?php }while($row = $result->fetch_assoc()); ?>
+    </table>
+</body>
+</html>
+{% endblock %}
